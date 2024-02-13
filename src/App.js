3511609665f895327348b55b601b1738549c1a0a
Chau Tieu <>
2024-02-13 14:34:07 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br />Hi there!
+          <br />Hi there! Bye there!
         </p>
         <a
           className="App-link"
